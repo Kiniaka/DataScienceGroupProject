b2560af8624bab2b0d65c3a19c6da600fc2d09a5
Kiniaka <147816937+Kiniaka@users.noreply.github.com>
2024-11-08 22:27:45 +0100

    update repo - echo

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Wyświetlenie adresu aplikacji
-echo "Aplikacja działa pod adresem: http://127.0.0.1:8000"
+echo "Poczekaj na INFO: Application startup complete. Wtedy aplikacja zacznie działać pod adresem: http://127.0.0.1:8000"
 # Uruchomienie serwera Uvicorn dla aplikacji FastAPI
 uvicorn main:app --host 0.0.0.0 --port 8000
 
