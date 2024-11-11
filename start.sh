437d07066a3c2a066b08408c8ed51de9e5203541
RyszardKozik <149445001+RyszardKozik@users.noreply.github.com>
2024-11-11 18:23:02 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 # Wyświetlenie adresu aplikacji
 echo "Poczekaj na INFO: Application startup complete. Wtedy aplikacja zacznie działać pod adresem: http://127.0.0.1:8000"
 # Uruchomienie serwera Uvicorn dla aplikacji FastAPI
